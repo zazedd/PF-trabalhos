9b2b53e3f53011d8e1e497344ab06e1d6dddd156
zazedd <leomendesantos@gmail.com>
2022-04-19 21:24:18 +0100

    up

--- trabB/probB.ml
+++ trabB/probB.ml
@@ -24,14 +24,6 @@ let rot_left = function
       make_node (make_node l1 v1 l2) v2 r1
   | _ -> failwith "rotation on leaf is not supported"
 
-(** using quick sort (partitioning and pivotal comparison) to sort list *)
-let rec quicksort = function
-  | [] -> []
-  | x :: xs -> begin
-    let sml, lrg = List.partition (fun y -> y < x) xs
-    in quicksort sml @ (x :: quicksort lrg)
-  end
-
 (** simple validation of first input *)
 let validate x = if x < 0 || x > 10000 then invalid_arg "Invalid number of elements in tree (0-10000)" else x
 
@@ -45,19 +37,19 @@ let rec make_big_list = function
   | 0 -> []
   | n -> (validate (read_int ()) |> make_list) :: make_big_list (n - 1)
 
-let num_trees = read_int ()
+let num_trees = 
+  let tmp = read_int () in 
+  if tmp < 0 || tmp > 5000 then invalid_arg "Invalid number of trees (0-5000)" else tmp
 
-let list = 
-  if num_trees < 0 || num_trees > 5000 then invalid_arg "Invalid number of trees (0-5000)" 
-  else num_trees |> make_big_list
+let list = num_trees |> make_big_list
 
 let timestart = Sys.time ()
 
-let () = List.iter (Printf.printf "| %d\n") (quicksort (List.nth list 0))
+let () = List.iter (Printf.printf "| %d\n") (List.fast_sort compare (List.nth list 0))
 let () = Printf.printf "\n"
-let () = List.iter (Printf.printf "| %d\n") (quicksort (List.nth list 1))
+let () = List.iter (Printf.printf "| %d\n") (List.fast_sort compare (List.nth list 1))
 let () = Printf.printf "\n"
-let () = List.iter (Printf.printf "| %d\n") (quicksort (List.nth list 2))
+let () = List.iter (Printf.printf "| %d\n") (List.fast_sort compare (List.nth list 2))
 
 let timeend = Sys.time ()
 
